f7171823ca24b32c9a78b2aff0d2318b31bb228c
Elias Haugsbakk <elias.haugsbakk@gmail.com>
2026-01-28 11:09:38 +0100

    Move backend into web-server and create new directory for uploader client

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO pages (id, title, slug, createdAt, markdown, html)
-VALUES
-    (1, 'Home', 'home', CURRENT_TIMESTAMP, 'Welcome to the home page.', '<p>Welcome to the home page.</p>'),
-    (2, 'About', 'about', CURRENT_TIMESTAMP, 'This wiki is about the project.', '<p>This wiki is about the project.</p>');
